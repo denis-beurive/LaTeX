6c258c02f7e7f12bf5c0b6eaac4438819d0062d1
Denis BEURIVE <denis.beurive@gmail.com>
2018-07-02 22:39:16 +0200

    Put the configuration outside the functions that use it. Fix a bug that appeared while editing empty files.

--- bash/latex.sh
+++ bash/latex.sh
@@ -2,13 +2,35 @@
 #
 # This file contains functions that can be used to customise your BASH environment.
 #
-# These functions should be added to your BASH configuration file ("~/.bashrc"). One way to do this s simply to
-# copy paste the content of this file to your BASH configuration file. Or you can source the present file from your
-# BASH configuration file.
+# These functions should be added to your BASH configuration file ("~/.bashrc").
+# One way to do this is simply to copy paste the content of this file to your BASH
+# configuration filei (~/.bashrc). Or you can source the present file from your BASH
+# configuration filei (~/.bashrc).
 #
 # WARNING !!!
 #
-# Some functions need to be configured. Please read the functions descriptions carefully.
+# Please, customize the function latex_configuration(). 
+
+##########################################################################
+#                            CONFIGURATION                               #
+##########################################################################
+
+# This funcion configures the LaTeX environment.
+# @note You must configure the function by setting values for the local variables listed below:
+#       - EXPECTED_ENC: list the accepted file encodings.
+#       - WANTED_ENC:   the encoding that you want to open the file with.
+# @see https://github.com/denis-beurive/LaTeX
+# @see http://www.iana.org/assignments/character-sets/character-sets.xhtml
+
+latex_configuration() {
+   type EXPECTED_ENC 2>&1 >/dev/null | egrep ' not found$' > /dev/null
+   if [ $? -eq 0 ]; then
+      export EXPECTED_ENC='ISO-8859-15|ISO-8859-1|ascii|us-ascii'
+      export WANTED_ENC='ISO-8859-15'
+   fi
+}
+
+latex_configuration
 
 # Make sure that the functions defined in this file are not already defined (for other purposes).
 
@@ -49,28 +71,19 @@ check_file_encoding() {
 # list of characters sets. If so, then the function launches the VIM editor on the given file, with a configured file
 # encoding.
 # @param [string] #1 Path the the file.
-# @note You must configure the function by setting values for the local variables listed below:
-#       - EXPECTED_ENC: list the accepted file encodings.
-#       - WANTED_ENC:   the encoding that you want to open the file with.
-# @see https://github.com/denis-beurive/LaTeX
-# @see http://www.iana.org/assignments/character-sets/character-sets.xhtml
 
 vitex() {
-
-   # CONFIGURATION
-
-   typeset -r EXPECTED_ENC='ISO-8859-15|ISO-8859-1|ascii|us-ascii'
-   typeset -r WANTED_ENC='ISO-8859-15'
-   
    typeset -r INPUT_FILE="$1"
-   typeset ENC
+   typeset ENC="${WANTED_ENC}"
    typeset MATCH="yes"
 
-   if [ -f "${INPUT_FILE}" ]; then
+   if [ -s "${INPUT_FILE}" ]; then
+      # The file exists and is not empty.
       ENC=$(file --mime-encoding "${INPUT_FILE}" | sed -e 's/^[^:]\+: \+//')
       MATCH=$(check_file_encoding "${ENC}" "${EXPECTED_ENC}")
    else
-      touch "${INPUT_FILE}"
+      # The file does not exist, or is empty.
+      touch "${INPUT_FILE}" 
    fi
 
    if [ -f "${INPUT_FILE}" ]; then   
@@ -80,7 +93,7 @@ vitex() {
            echo "Wrong file encoding (${ENC}) ! Accepted encodings are: ${EXPECTED_ENC}"
       fi
    else 
-      echo "Something is wrong with the given path! This is not a regular file!"
+      echo "Something is wrong with the given path (${INPUT_FILE})! This is not a regular file!"
    fi
 }
 
