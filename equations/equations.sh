561b21bcca9fb348cf9f66b3d1ccde71e2ba8d29
Denis BEURIVE <dbeurive@protonmail.com>
2021-09-16 19:06:53 +0200

    Handle more than one PDF document

--- equations/equations.sh
+++ equations/equations.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-set -x
+set +x
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 readonly __DIR__="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-readonly ROOT="${__DIR__}/../../.."
+readonly ROOT="${__DIR__}"
 
 readonly PDFLATEX="/home/denis/.texlive/bin/x86_64-linux/pdflatex"
-readonly PDF_OUT="${ROOT}/out"
+readonly PDF_OUT="${ROOT}/latex-out"
 
 if [ ! -d "${PDF_OUT}" ]; then
   mkdir "${PDF_OUT}" || exit 1
@@ -15,22 +15,22 @@ fi
 
 "${PDFLATEX}" -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory="${PDF_OUT}" equations.tex
 if [ $? -eq 0 ]; then
-  echo "SUCCESS"
+  echo "LaTex: SUCCESS"
 else
-  echo "FAILURE"
+  echo "LaTex: FAILURE"
   exit 1
 fi
 
-TMP_IMAGE="${__DIR__}/equations-tmp.png"
-
-# Crop.
-# If you want to convert the background color from transparent to white, then add these options:
-# -background white -alpha remove
-convert  -trim -density 300 "${PDF_OUT}/equations.pdf" -quality 90 "${TMP_IMAGE}"
+# Convert the PDF into PNG.
+# Warning: this operation may produce more that one PNG image.
+#          It produces as many images as there are pages in the PDF document.
+# Note: if you want to convert the background color from transparent to white, then add these options:
+#       -background white -alpha remove
+convert -background white -alpha remove -trim -density 300 "${PDF_OUT}/equations.pdf" -quality 90 "${__DIR__}/equations-tmp.png"
 if [ $? -eq 0 ]; then
-  echo "SUCCESS"
+  echo "convert PDF -> PNG: SUCCESS"
 else
-  echo "FAILURE"
+  echo "convert PDF -> PNG: FAILURE"
   exit 1
 fi
 
@@ -39,12 +39,17 @@ fi
 #     -bordercolor transparent
 # by:
 #     -bordercolor white
-convert -border 20 -bordercolor transparent "${TMP_IMAGE}" "${__DIR__}/equations.png"
-if [ $? -eq 0 ]; then
-  echo "SUCCESS"
-else
-  echo "FAILURE"
-  exit 1
-fi
 
-rm "${TMP_IMAGE}"
+for image in $(find . -maxdepth 1 | perl -e '@v=(); while(<>) { chomp;  push @v, $_ if ($_ =~ m/\/equations-tmp(-\d+)?\.png$/)} print join " ", @v'); do
+    target=$(printf "%s" "${image}" | perl -pe 's/-tmp((-\d+)\.png)$/$1/')
+    convert -border 20 -bordercolor white "${image}" "${target}"
+    if [ $? -eq 0 ]; then
+      echo "convert (${image}): SUCCESS"
+      rm ${image}
+    else
+      echo "convert (${image}): FAILURE"
+      exit 1
+    fi
+done
+
+
